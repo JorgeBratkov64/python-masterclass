7ec49d81d37a65aef7d785975e24541080392977
Unknown <jorge.cervantes.gtz@gmail.com>
2018-12-30 21:47:40 -0600

    Section 5: The basics of Python
    Class Exercises for section 5

---
+++ 26_VariablesAndStirngs.py
@@ -0,0 +1,45 @@
+a = 12
+b = 3
+c = a + b
+d = c / 3
+e = d - 4
+print(e * 12)
+
+parrot =  "Norwegian Blue"
+print(parrot)
+print(parrot[0])
+print(parrot[1])
+print(parrot[2])
+print(parrot[3])
+print(parrot[4])
+print(parrot[-1])
+print(parrot[0:10])
+print(parrot[:15])
+print(parrot[10:])
+# Characters from 0 to 6 in step of 2
+print(parrot[0:6:2])
+# Characters from 0 to 6 in step of 3
+print(parrot[0:6:3])
+
+number = "9,223,372,036,854,755,807"
+print(number[1::4])
+numbers = "1, 2, 3, 4, 5, 6, 7, 8, 9"
+print(numbers[0::3])
+
+print()
+string1 = "he's"
+string2 = "probably"
+print(string1 + ' '  + string2)
+print("he's probably pining")
+print("Hello " * 5)
+print("Hello " * (5 + 4))
+print("Hello  " * 5 + ' 4')
+
+print()
+today = "Friday"
+print("day" in today)
+print("Fri" in today)
+print("thur" in today)
+print("parrot" in "Norwegian")
+
+
